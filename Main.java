5192ffde98cece34e59cf9294cafd89ae820c55c
AnthonyValino22 <135483597+AnthonyValino22@users.noreply.github.com>
2023-06-25 17:11:20 +0000

    Updated code to function as intended

--- Main.java
+++ Main.java
@@ -1,7 +1,16 @@
+import java.util.Scanner;
+
 class Main {
   public static void main(String[] args) {
     System.out.println("Enter a word:");
-    
-    //Your code will return the length of the entered String
-  }
+    Scanner scan = new Scanner(System.in);
+
+    String userName = scan.nextLine();
+      
+      int length = userName.length();
+      System.out.println("The length of the word is: " + length);
+    }
 }
+    //Your code will return the length of the entered String
+  
+
